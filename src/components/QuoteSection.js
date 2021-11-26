2bd3fc87f55c9aeaa34349bbd6b4b262d0b667e6
Sevda <sevda.amini@gmail.com>
2021-11-26 10:01:21 +1000

    playing with Rapid APIs

--- src/components/QuoteSection.js
+++ src/components/QuoteSection.js
@@ -1,29 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
 const QuoteSection = () => {
-    const [quote, setQuote] = useState('')
-    let options = {
-        method: 'GET',
-        url: 'https://healthruwords.p.rapidapi.com/v1/quotes/',
-        params: {t: 'Wisdom', maxR: '1', size: 'medium', id: '731'},
-        headers: {
-          'x-rapidapi-host': process.env.REACT_APP_API_KEY,
-          'x-rapidapi-key': process.env.REACT_APP_API_HOST
-        }
-      };
+  
 
-    
+  return (
+    <div>
+       
+      
+    </div>
+  );
+};
 
-    const getQuote = (options) => {
-        fetch('https://healthruwords.p.rapidapi.com/v1/quotes/')
-        .then(res => res.json())
-        .then(data => setQuote(data.content))
-    }
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default QuoteSection
+export default QuoteSection;
